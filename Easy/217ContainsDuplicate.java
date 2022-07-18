9ed12d8f156f1a0c26419a789fd9126ae06cdbf7
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-07-18 14:15:32 +0200

    Create 217ContainsDuplicate.java

---
+++ Easy/217ContainsDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (map.containsKey(nums[i])) {
+                return true;
+            }
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
