6166e75f2e84472b8cf463dde04b74ad1efc9a90
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-07-19 16:22:48 +0200

    Create 141LinkedListCycle.py

---
+++ Easy/141LinkedListCycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        all_the_nodes = set()
+        
+        while head:
+            if head in all_the_nodes:
+                return True
+            else:
+                all_the_nodes.add(head)
+            head = head.next
+            
+        return False
+    
