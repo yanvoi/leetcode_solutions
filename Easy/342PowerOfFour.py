6cc926a8c06c635c3971be030e51bdca3f0e90ac
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-07-19 14:03:29 +0200

    Create 342PowerOfFour.py

---
+++ Easy/342PowerOfFour.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        
+        while n > 1:
+            n = n / 4
+            
+        if n == 1:
+            return True
+        
+        return False
+        
