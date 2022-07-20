676b50cba374ae57d937e40f91c8fc8778f892fc
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-07-20 16:53:32 +0200

    Create 2236RootEqualsSumOfChildren.py

---
+++ Easy/2236RootEqualsSumOfChildren.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        
+        if root.val == root.left.val + root.right.val:
+            return True
+        return False
+        
