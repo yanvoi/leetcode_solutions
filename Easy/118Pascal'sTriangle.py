6b4e2882adb7e24b4fb7f3ffc91b966e28e9a23c
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-07-21 16:29:57 +0200

    Create 118Pascal'sTriangle.py

---
+++ Easy/118Pascal'sTriangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+        answer = []
+        
+        for row in range(numRows):
+            answer.append([])
+            for col in range(row + 1):
+                if col == 0 or col == row:
+                    answer[row].append(1)
+                else:
+                    answer[row].append(answer[row-1][col-1]+answer[row-1][col])
+                    
+        return answer
+        
