75497e95a961af67df902b37c4ea024bfdb2e632
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-04 13:05:40 +0200

    Create 116PopulatingNextRightPointersInEachNode.py

---
+++ Medium/116PopulatingNextRightPointersInEachNode.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not root:
+            return
+        
+        pre = root
+        cur = None
+        
+        while pre.left:
+            cur = pre
+            while cur:
+                cur.left.next = cur.right
+                if cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            pre = pre.left
+            
+        return root
+    
