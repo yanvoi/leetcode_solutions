3fc1c83b561806bb70b584b43907f8c6a7f011f1
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-06 23:07:57 +0200

    Create 944DeleteColumnsToMakeSorted.py

---
+++ Easy/944DeleteColumnsToMakeSorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        count = 0
+        
+        for col in range(len(strs[0])):
+            for row in range(len(strs)-1):
+                if ord(strs[row][col]) > ord(strs[row+1][col]):
+                    count +=1
+                    break
+                    
+        return count
+    
