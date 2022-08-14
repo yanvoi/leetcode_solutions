7be0ddcbd78e58e4b0404956d4eab8b8903f549a
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-14 12:34:13 +0200

    Update 20ValidParentheses.py

--- Easy/20ValidParentheses.py
+++ Easy/20ValidParentheses.py
@@ -17,11 +17,11 @@ class Solution:
                         return False
                     
                     if char == ')' and stack[-1] == '(':
-                        stack = stack[:-1]
+                        stack.pop()
                     elif char == '}' and stack[-1] == '{':
-                        stack =stack[:-1]
+                        stack.pop()
                     elif char == ']' and stack[-1] == '[':
-                        stack = stack[:-1]
+                        stack.pop()
                     else:
                         return False
                         
