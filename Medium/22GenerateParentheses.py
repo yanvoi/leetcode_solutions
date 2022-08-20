ebdcb8e890007d0f8463083ff7d161e734b25ec6
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-20 14:01:50 +0200

    Create 22GenerateParentheses.py

---
+++ Medium/22GenerateParentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        ans = []
+        
+        def backtracking(path, left, right, ans):
+            
+            if len(path) == 2 * n:
+                ans.append(''.join(path))
+                return
+            
+            if left < n:
+                path.append('(')
+                backtracking(path, left+1, right, ans)
+                path.pop()
+                
+            if right < left:
+                path.append(')')
+                backtracking(path, left, right+1, ans)
+                path.pop()
+    
+        backtracking([], 0, 0, ans)
+        return ans
