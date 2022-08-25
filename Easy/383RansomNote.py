ad5d4c71905512947df87d93925dd9aa4a471d3e
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-25 19:40:42 +0200

    Update 383RansomNote.py

--- Easy/383RansomNote.py
+++ Easy/383RansomNote.py
@@ -1,26 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
-        available_nums = dict()
+        available = dict()
         for char in magazine:
-            if char in available_nums:
-                available_nums[char] += 1
-            else:
-                available_nums[char] = 1
-                
-        needed_nums = dict()
+            available[char] = available.get(char, 0) + 1
+            
+        needed = dict()
         for char in ransomNote:
-            if not char in available_nums:
-                return False
-            
-            if char in needed_nums:
-                needed_nums[char] += 1
-            else:
-                needed_nums[char] = 1
-                
-        for letter in needed_nums:
-            if needed_nums[letter] > available_nums[letter]:
-                return False
-            
+            if char not in available: return False
+            needed[char] = needed.get(char, 0) + 1
+        
+        for char in needed:
+            if needed[char] > available[char]: return False
+        
         return True
         
