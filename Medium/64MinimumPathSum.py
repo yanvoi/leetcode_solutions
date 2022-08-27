292af25bc58ba30015e4bb271975f8f406630c4d
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-08-27 14:14:00 +0200

    Update 64MinimumPathSum.py

--- Medium/64MinimumPathSum.py
+++ Medium/64MinimumPathSum.py
@@ -5,11 +5,11 @@ class Solution:
             for j in range(len(grid[0])):
                 
                 if i > 0 and j > 0:
-                    grid[i][j] = min(grid[i][j] + grid[i-1][j], grid[i][j] + grid[i][j-1])
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
                 elif i > 0:
-                    grid[i][j] = grid[i][j] + grid[i-1][j]
+                    grid[i][j] += grid[i-1][j]
                 elif j > 0:
-                    grid[i][j] = grid[i][j] + grid[i][j-1]
+                    grid[i][j] += grid[i][j-1]
                     
         return grid[-1][-1]
     
