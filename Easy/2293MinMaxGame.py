d7be12214f549f88985526bd90fe2f5ece5b46cb
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-09-10 13:50:26 +0200

    Create 2293MinMaxGame.py

--- Easy/2293MinMaxGame.py
+++ Easy/2293MinMaxGame.py
@@ -1,16 +1,17 @@
 class Solution:
     def minMaxGame(self, nums: List[int]) -> int:
         
-        while len(nums) > 1:
-            n = len(nums) // 2
+        divide = 1
+        
+        while len(nums) // divide > 1:
+            divide = divide * 2
+            n = len(nums) // divide
             
             for i in range(n):
                 if i % 2 == 0:
                     nums[i] = min(nums[2 * i], nums[2 * i + 1])
                 else:
                     nums[i] = max(nums[2 * i], nums[2 * i + 1])
-                    
-            nums = nums[:n]
             
         return nums[0]
         
