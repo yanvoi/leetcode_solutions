ba9bee213fca2001e79238ed1d106005bfbc3a27
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-09-26 22:52:32 +0200

    Update 551StudentAttendanceRecordI.py

--- Easy/551StudentAttendanceRecordI.py
+++ Easy/551StudentAttendanceRecordI.py
@@ -4,8 +4,8 @@ class Solution:
         num_of_As, consecutive_Ls = 0, 0
         
         for char in s:
-            if char == 'A': num_of_As += 1
-            elif char == 'L': consecutive_Ls += 1
+            if char == 'L': consecutive_Ls += 1
+            elif char == 'A': num_of_As += 1
                 
             if num_of_As >= 2 or consecutive_Ls >= 3: return False
             
