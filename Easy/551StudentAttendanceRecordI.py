0e2dea7f6a527addd144040a3dadea7b4ed217a3
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-09-26 22:50:55 +0200

    Create 551StudentAttendanceRecordI.py

---
+++ Easy/551StudentAttendanceRecordI.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        
+        num_of_As, consecutive_Ls = 0, 0
+        
+        for char in s:
+            if char == 'A': num_of_As += 1
+            elif char == 'L': consecutive_Ls += 1
+                
+            if num_of_As >= 2 or consecutive_Ls >= 3: return False
+            
+            if char != 'L': consecutive_Ls = 0
+            
+        return True
+        
