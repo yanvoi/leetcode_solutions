0bf33d79049573901be5b5c75e242170999e6c04
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-09-29 16:15:53 +0200

    Create 696CountBinarySubstrings.py

---
+++ Easy/696CountBinarySubstrings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        
+        ans, prev, cur = 0, 0, 1
+        
+        for i in range(1, len(s)):
+            if s[i] != s[i-1]:
+                ans += min(prev, cur)
+                prev, cur = cur, 1
+            else:
+                cur += 1
+
+        return ans + min(prev, cur)
+        
