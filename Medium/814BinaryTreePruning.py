e86ed05a4819e150fbeba70d20cf6a8cda5529c9
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-04 11:33:05 +0200

    Create 814BinaryTreePruning.py

--- Medium/814BinaryTreePruning.py
+++ Medium/814BinaryTreePruning.py
@@ -7,19 +7,17 @@
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        def has_one(node):
+        def contains_one(node):
             
             if not node: return False
             
-            left = has_one(node.left)
-            if not left:
-                node.left = None
-                
-            right = has_one(node.right)
-            if not right:
-                node.right = None
+            left = contains_one(node.left)
+            right = contains_one(node.right)
+            
+            if not left: node.left = None
+            if not right: node.right = None
                 
             return node.val == 1 or left or right
         
-        return root if has_one(root) else None
+        return root if contains_one(root) else None
         
