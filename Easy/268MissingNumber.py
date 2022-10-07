04acacefe9cc1145f20a66ee38131a4a22845a09
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-07 15:57:10 +0200

    Update 268MissingNumber.py

--- Easy/268MissingNumber.py
+++ Easy/268MissingNumber.py
@@ -1,12 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        expected = (1 + len(nums)) * len(nums) // 2
-        
-        sum = 0
-        
-        for i in range(len(nums)):
-            sum += nums[i]
-            
-        return expected - sum
+        return (len(nums) * (len(nums) + 1) // 2) - sum(nums)
         
