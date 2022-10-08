3055b5ca764ede75ef962d73a8a6aa2a9a4aa2fc
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-08 12:08:33 +0200

    Create 16_3SumClosest.py

--- Medium/16_3SumClosest.py
+++ Medium/16_3SumClosest.py
@@ -2,24 +2,25 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         
         nums.sort()
-        ans = nums[0] + nums[1] + nums[2]
+        ans = float('inf')
         
         for left in range(len(nums) - 2):
+            if left > 0 and nums[left-1] == nums[left]: continue
+            
             mid, right = left + 1, len(nums) - 1
             
             while mid < right:
-                cur_sum = nums[left] + nums[mid] + nums[right]
+                cur = nums[left] + nums[mid] + nums[right]
                 
-                if cur_sum == target:
-                    return cur_sum
+                if cur == target: return target
                 
-                if abs(cur_sum - target) < abs(ans - target):
-                    ans = cur_sum
+                if abs(target - cur) < abs(target - ans):
+                    ans = cur
                     
-                if cur_sum > target:
-                    right -= 1
-                else:
+                if target > cur:
                     mid += 1
+                else:
+                    right -= 1
                     
         return ans
         
