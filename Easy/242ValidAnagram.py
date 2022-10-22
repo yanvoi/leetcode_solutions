8574dfafa3fc88c7bd5e17eaa69e4374d44a03a5
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-22 19:26:09 +0200

    Update 242ValidAnagram.py

--- Easy/242ValidAnagram.py
+++ Easy/242ValidAnagram.py
@@ -8,19 +8,13 @@ class Solution:
         second = dict()
         
         for letter in s:
-            if letter in first:
-                first[letter] += 1
-            else:
-                first[letter] = 1
-                
-        for letter in t:
-            if letter in second:
-                second[letter] += 1
-            else:
-                second[letter] = 1
+            first[letter] = first.get(letter, 0) + 1
             
-        for i in first:
-            if not i in second or not first[i] == second[i]:
+        for letter in t:
+            second[letter] = second.get(letter, 0) + 1
+        
+        for letter in first:
+            if letter not in second or first[letter] != second[letter]:
                 return False
             
         return True
