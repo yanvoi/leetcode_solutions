0c0d6454e507875ebb7530b42106770719138044
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-25 12:36:55 +0200

    Update 1662CheckIfTwoStringArraysAreEquivalent.py

--- Easy/1662CheckIfTwoStringArraysAreEquivalent.py
+++ Easy/1662CheckIfTwoStringArraysAreEquivalent.py
@@ -1,5 +1,25 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         
-        return ''.join(word1) == ''.join(word2)
+        for c1, c2 in zip(self.__generate__(word1), self.__generate__(word2)):
+            if c1 != c2:
+                return False
+            
+        return True
+    
+    
+    # generators allow us to solve the problem with O(1) space complexity
+    def __generate__(self, arr):
         
+        for word in arr:
+            for char in word:
+                yield char
+                
+        yield None
+    
+    
+ class SecondSolution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        
+        return ''.join(word1) == ''.join(word2)
+    
