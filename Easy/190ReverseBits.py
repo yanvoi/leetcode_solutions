3fa7b916af5f72b6fee8a09f675280d3e278445d
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-31 09:21:50 +0100

    Create 190ReverseBits.py

--- Easy/190ReverseBits.py
+++ Easy/190ReverseBits.py
@@ -1,12 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         
-        answer = 0
+        answer = int(0)
         
         for i in range(32):
             
             bit = (n >> i) & 1
-            answer = answer | (bit << (31-i))
+            answer = answer | (bit << (31- i))
             
         return answer
         
