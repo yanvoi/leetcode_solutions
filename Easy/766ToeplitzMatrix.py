01c6349019d7f11881ddbc4b7d4c3b3666c3606c
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-10-31 09:14:47 +0100

    Update 766ToeplitzMatrix.py

--- Easy/766ToeplitzMatrix.py
+++ Easy/766ToeplitzMatrix.py
@@ -2,26 +2,28 @@ class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         
         for i in range(len(matrix)):
+            
             val = matrix[i][0]
             x = i + 1
             y = 1
             
             while x < len(matrix) and y < len(matrix[0]):
-                if matrix[x][y] != val: return False
+                if matrix[x][y] != val:
+                    return False
                 x += 1
                 y += 1
                 
-        for i in range(1, len(matrix[0])):
+        for i in range(len(matrix[0])):
             
             val = matrix[0][i]
-            
             x = 1
             y = i + 1
             
             while x < len(matrix) and y < len(matrix[0]):
-                if matrix[x][y] != val: return False
-                
+                if matrix[x][y] != val:
+                    return False
                 x += 1
                 y += 1
-                
+            
         return True
+        
