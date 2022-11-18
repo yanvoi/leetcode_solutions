004ddfb8291f8fdda8ebf6c7885628fa01d3b91d
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-11-18 10:06:14 +0100

    Update 263UglyNumber.py

--- Easy/263UglyNumber.py
+++ Easy/263UglyNumber.py
@@ -1,19 +1,12 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
         
-        if n <= 0:
-            return False
-        
-        while n != 1:
-            
+        while n > 0 and (n % 2 == 0 or n % 3 == 0 or n % 5 == 0):
             if n % 2 == 0:
-                n = n // 2
-            elif n % 3 == 0:
-                n = n // 3
-            elif n % 5 == 0:
-                n = n // 5
-            else:
-                return False
-        
-        return True
-        
+                n //= 2
+            if n % 3 == 0:
+                n //= 3
+            if n % 5 == 0:
+                n //= 5
+                
+        return n == 1
