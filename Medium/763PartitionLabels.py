85edf049aa83d614e1180daab7c401723e175abf
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-11-26 23:38:52 +0100

    Update 763PartitionLabels.py

--- Medium/763PartitionLabels.py
+++ Medium/763PartitionLabels.py
@@ -2,23 +2,19 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
         # O(n) time complexity
-        # indices: left most and right most occurence in the string
-        lr_occurence = dict()
+        # holds the index of the last occurence of each character
+        last_occurence = dict()
         
         for index, char in enumerate(s):
-            if char in lr_occurence:
-                lr_occurence[char][1] = index
-            else:
-                lr_occurence[char] = [index, index]
+            last_occurence[char] = index
             
         end, ans = 0, []
         for index, char in enumerate(s):
-            if index >= end and lr_occurence[char][0] == lr_occurence[char][1]:
+            if end < last_occurence[char]:
+                end = last_occurence[char]
+                
+            if end == index and last_occurence[char] == index:
                 ans.append(index + 1)
-            elif lr_occurence[char][1] > end:
-                end = lr_occurence[char][1]
-            elif lr_occurence[char][1] <= end and index == end:
-                ans.append(end + 1)
                 
         for i in range(len(ans) - 1, 0, -1):
             ans[i] -= ans[i-1]
