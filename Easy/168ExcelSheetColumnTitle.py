5d06f9b85dd8eb9490f27fc5798f1bf757d03e90
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-11-30 10:07:17 +0100

    Update 168ExcelSheetColumnTitle.py

--- Easy/168ExcelSheetColumnTitle.py
+++ Easy/168ExcelSheetColumnTitle.py
@@ -1,11 +1,11 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         letters = [chr(x) for x in range(ord('A'), ord('Z') + 1)]
-        answer = ''
+        answer = []
         
         while columnNumber > 0:
-            answer = letters[(columnNumber % 26) - 1] + answer
+            answer.append(letters[(columnNumber % 26) - 1])
             columnNumber = (columnNumber - 1) // 26
             
-        return ''.join(answer)
+        return ''.join(answer[::-1])
         
