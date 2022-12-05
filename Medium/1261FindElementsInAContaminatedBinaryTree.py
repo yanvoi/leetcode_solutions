c8f126d2fc4d53d6b2556c39ae9dc018c64051b2
yanvoi <96974442+yanvoi@users.noreply.github.com>
2022-12-05 18:55:17 +0100

    Create 1261FindElementsInAContaminatedBinaryTree.py

---
+++ Medium/1261FindElementsInAContaminatedBinaryTree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        
+        self.root = root
+        self.root.val = 0
+        self.available = set()
+        
+        q = collections.deque()
+        q.append(self.root)
+        
+        while q:
+            
+            q_size = len(q)
+            for _ in range(q_size):
+                node = q.popleft()
+                self.available.add(node.val)
+                
+                if node.left:
+                    node.left.val = 2 * node.val + 1
+                    q.append(node.left)
+                if node.right:
+                    node.right.val = 2 * node.val + 2
+                    q.append(node.right)
+                
+
+    def find(self, target: int) -> bool:
+        return target in self.available
+        
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
