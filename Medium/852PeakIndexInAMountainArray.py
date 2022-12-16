4606c120c58d9def7c71ad54ad7f8b45a519cc9e
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2022-12-16 21:38:58 +0100

    Create 852PeakIndexInAMountainArray.py

---
+++ Medium/852PeakIndexInAMountainArray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+
+        left, right = 1, len(arr) - 2
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid-1] < arr[mid] > arr[mid+1]:
+                return mid
+
+            if arr[mid] > arr[mid-1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
