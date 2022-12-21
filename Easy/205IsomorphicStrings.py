de435af0386c3b92fb9f27ae0e1527c24d1a65e4
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2022-12-21 12:44:15 +0100

    Update 205IsomorphicStrings.py

--- Easy/205IsomorphicStrings.py
+++ Easy/205IsomorphicStrings.py
@@ -1,26 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t):
-            return False
-        
-        mapping = dict()
-        for i in range(len(s)):
-            if s[i] in mapping:
-                mapping[s[i]].append(i)
-            else:
-                mapping[s[i]] = [i]
-                
-        second = dict()
-        for i in range(len(t)):
-            if t[i] in second:
-                second[t[i]].append(i)
-            else:
-                second[t[i]] = [i]
-                
-        for el in mapping.values():
-            if not el in second.values():
+
+        s_to_t = dict()
+        t_to_s = dict()
+        for c1, c2 in zip(s, t):
+            if (c1 in s_to_t and s_to_t[c1] != c2) or (c2 in t_to_s and t_to_s[c2] != c1):
                 return False
-        
-        return True
-        
+            
+            s_to_t[c1] = c2
+            t_to_s[c2] = c1
+
+        return len(s) == len(t)
