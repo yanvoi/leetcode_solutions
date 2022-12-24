9fa9ee27e8333edef1dd88ffa44cb458f91d3bb9
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2022-12-24 13:00:57 +0100

    Create 131PalindromePartitioning.py

--- Medium/131PalindromePartitioning.py
+++ Medium/131PalindromePartitioning.py
@@ -3,20 +3,20 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
 
         self.s = s
-        answer = []
-        self.__dfs__(0, [], answer)
-        return answer
+        self.answer = []
+        self.__dfs__(0, [])
+        return self.answer
 
 
-    def __dfs__(self, index, cur_s, answer):
+    def __dfs__(self, index, cur_s):
         if index == len(self.s):
-            answer.append(cur_s)
+            self.answer.append(cur_s)
             return
 
         for i in range(index+1, len(self.s) + 1):
             next_bit = self.s[index:i]
             if self.__is_palindrome__(next_bit):
-                self.__dfs__(i, cur_s + [next_bit], answer)
+                self.__dfs__(i, cur_s + [next_bit])
 
 
     def __is_palindrome__(self, s):
