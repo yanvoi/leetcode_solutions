04c49399514d1a810617412ecfdab91e546bf58a
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2022-12-24 23:11:47 +0100

    Create 2514CountAnagrams.py

---
+++ Hard/2514CountAnagrams.py
@@ -0,0 +1,27 @@
+# for each word in the string
+# we count the number of unique permutations of characters in this word
+# and return the product of the result
+
+class Solution:
+    def countAnagrams(self, s: str) -> int:
+        
+        answer = 1
+        words = s.split()
+        for word in words:
+            answer *= self.count_unique_permutations(word)
+            
+        return answer % (10**9 + 7)
+    
+    
+    def count_unique_permutations(self, s):
+        answer = math.factorial(len(s))
+        
+        freq = dict()
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+            
+        for num in freq.values():
+            answer //= math.factorial(num)
+            
+        return answer
+        
