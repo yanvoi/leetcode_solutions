f8d95f0b380f9a3e0345eaf790f0521bd44af755
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2022-12-26 13:06:53 +0100

    Create 413ArithmeticSlices.py

---
+++ Medium/413ArithmeticSlices.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        
+        dp = [0] * len(nums)
+
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp[i] = dp[i-1] + 1
+
+        return sum(dp)
