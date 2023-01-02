6ea52df7712b9ad42e1a42da0791ace04195f44f
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-01-02 10:19:15 +0100

    Update 520DetectCapital.py

--- Easy/520DetectCapital.py
+++ Easy/520DetectCapital.py
@@ -6,8 +6,5 @@ class Solution:
             if letter.isupper():
                 count += 1
                 
-        if count == len(word) or (count == 1 and word[0].isupper()) or count == 0:
-            return True
-        
-        return False
+        return count == 0 or count == len(word) or (count == 1 and word[0].isupper())
         
