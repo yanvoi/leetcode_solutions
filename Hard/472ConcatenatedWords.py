28c722586ce05275b025da22f34fb53a755118cb
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-01-27 11:15:53 +0100

    Create 472ConcatenatedWords.py

---
+++ Hard/472ConcatenatedWords.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+
+        self.available = set(words)
+        result = []
+        for word in words:
+            if self.dfs(word):
+                result.append(word)
+
+        return result
+
+
+    @cache
+    def dfs(self, word):
+
+        for i in range(1, len(word)):
+
+            prefix = word[:i]
+            suffix = word[i:]
+
+            if prefix in self.available and suffix in self.available:
+                return True
+
+            if prefix in self.available and self.dfs(suffix):
+                return True
+
+            if suffix in self.available and self.dfs(prefix):
+                return True
+
+        return False
