5a84cbe572a93cfdc7717f6378a69c38ea023a3e
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-01-30 16:12:16 +0100

    Update 1TwoSum.py

--- Easy/1TwoSum.py
+++ Easy/1TwoSum.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        hash_map = dict()
-        
+        seen = dict()
         for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hash_map:
-                return [hash_map[diff], i]
-            
-            hash_map[num] = i
-            
-        return False
+            if target - num in seen:
+                return seen[target - num], i
+
+            seen[num] = i
         
