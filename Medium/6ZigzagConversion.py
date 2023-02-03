a1fbc967389a3c1e10ba477ce41cceca0d4f3f05
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-03 09:50:27 +0100

    Update 6ZigzagConversion.py

--- Medium/6ZigzagConversion.py
+++ Medium/6ZigzagConversion.py
@@ -1,21 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
-        if numRows <= 1: return s
-            
-        curRow, step = 0, 1
-        ans = [''] * numRows
+        if numRows == 1 or numRows >= len(s): return s
         
+        ans = [[] for _ in range(numRows)]
+        index, step = 0, 1
+
         for char in s:
-            
-            ans[curRow] += char
-            
-            if curRow == numRows - 1:
+            ans[index].append(char)
+            if index == numRows - 1:
                 step = -1
-            elif curRow == 0:
+            elif index == 0:
                 step = 1
-                
-            curRow += step
-            
-        return ''.join(ans)
+
+            index += step
+
+        return ''.join(''.join(row) for row in ans)
         
