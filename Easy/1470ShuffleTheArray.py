4342f93ecc73ae10428b561e62171b2d55b6cc88
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-06 08:24:55 +0100

    Update 1470ShuffleTheArray.py

--- Easy/1470ShuffleTheArray.py
+++ Easy/1470ShuffleTheArray.py
@@ -1,10 +1,9 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        
-        for i in range(0, len(nums), 2):
-            to_pop = nums.pop(i + n)
-            nums.insert(i + 1, to_pop)
-            n = n - 1
 
-        return nums
-    
+        answer = []
+        for x, y in zip(nums[:n], nums[n:]):
+            answer.append(x)
+            answer.append(y)
+
+        return answer
