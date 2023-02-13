e3f4d7103e18c05b632a5dd6f5740885c1bc95ad
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-13 23:27:27 +0100

    Create 1450NumberOfStudentsDoingHomeworkAtAGivenTime.py

---
+++ Easy/1450NumberOfStudentsDoingHomeworkAtAGivenTime.py
@@ -0,0 +1,5 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+
+        return sum(start <= queryTime <= end for start, end in zip(startTime, endTime))
+        
