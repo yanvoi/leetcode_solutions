f359fc3f120c5622146b8d1de48f578940f4c817
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-15 10:15:37 +0100

    Create 989AddToArray-FormOfInteger.py

--- Easy/989AddToArray-FormOfInteger.py
+++ Easy/989AddToArray-FormOfInteger.py
@@ -1,18 +1,9 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        
-        string = ""
-        for number in num:
-            string = string + str(number)
-            
-        number = int(string)
-        number += k
-        number = str(number)
-        
-        answer = []
-        
-        for char in number:
-            answer.append(char)
-            
-        return answer
-        
+
+        for i in range(len(num) - 1, -1, -1):
+            k += num[i]
+            num[i] = k % 10
+            k //= 10
+
+        return [int(digit) for digit in str(k)] + num if k > 0 else num
