c791977d365d6176af76e847fbb91f78d8014a04
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-15 19:32:33 +0100

    Update 2149RearrangeArrayElementsBySign.py

--- Medium/2149RearrangeArrayElementsBySign.py
+++ Medium/2149RearrangeArrayElementsBySign.py
@@ -1,11 +1,4 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
 
-        positive, negative = [], []
-        for num in nums:
-            if num >= 0:
-                positive.append(num)
-            else:
-                negative.append(num)
-
-        return [num for pair in zip(positive, negative) for num in pair]
+        return [num for pair in zip((i for i in nums if i >= 0), (j for j in nums if j < 0)) for num in pair]
