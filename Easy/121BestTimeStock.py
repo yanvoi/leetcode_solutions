b4bc60901817c61f779301d8b110d29b5b231e11
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-02-25 09:49:50 +0100

    Update 121BestTimeStock.py

--- Easy/121BestTimeStock.py
+++ Easy/121BestTimeStock.py
@@ -1,17 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        if len(prices) == 0:
-            return 0
-        
-        max_profit = 0
-        min_price = prices[0]
-        
-        for current in prices[1:]:
-            if current < min_price:
-                min_price = current
-            elif max_profit < current - min_price:
-                max_profit = current - min_price
-        
-        return max_profit
-    
+        profit, min_price = 0, float('inf')
+        for price in prices:
+            profit = max(profit, price - min_price)
+            min_price = min(min_price, price)
+
+        return profit
