1189e97358eac9161319a1ec290d2035c469491f
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-03-17 21:37:24 +0100

    Update 2352EqualRowAndColumnPairs.py

--- Medium/2352EqualRowAndColumnPairs.py
+++ Medium/2352EqualRowAndColumnPairs.py
@@ -2,11 +2,6 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
 
         count_row = Counter(tuple(row) for row in grid)
-        count_col = defaultdict(int)
-        for col in range(len(grid[0])):
-            column = []
-            for row in range(len(grid)):
-                column.append(grid[row][col])
-            count_col[tuple(column)] += 1
+        count_col = Counter(tuple(col) for col in zip(*grid))
 
         return sum(c1 * c2 for key1, c1 in count_row.items() for key2, c2 in count_col.items() if key1 == key2)
