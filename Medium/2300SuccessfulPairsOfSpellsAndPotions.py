61cf22467dcb9d434c4e5401069af510b6fa2d60
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-04-02 14:56:14 +0200

    Create 2300SuccessfulPairsOfSpellsAndPotions.py

---
+++ Medium/2300SuccessfulPairsOfSpellsAndPotions.py
@@ -0,0 +1,5 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+
+        potions.sort()
+        return [len(potions) - bisect.bisect_left(potions, success / spell) for spell in spells]
