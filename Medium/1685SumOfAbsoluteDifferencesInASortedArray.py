94f0f676a2bc83d14e85b59881cb3f8eff8db5f6
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-04-06 07:52:56 +0200

    Update 1685SumOfAbsoluteDifferencesInASortedArray.py

--- Medium/1685SumOfAbsoluteDifferencesInASortedArray.py
+++ Medium/1685SumOfAbsoluteDifferencesInASortedArray.py
@@ -1,10 +1,11 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
 
-        left_sum, right_sum, n, answer = 0, sum(nums), len(nums), []
+        left_sum, right_sum, answer = 0, sum(nums), []
 
         for i, num in enumerate(nums):
-            answer.append(num * i - left_sum + right_sum - num * (n - i))
+            # thanks to the list being sorted
+            answer.append(num * i - left_sum + right_sum - num * (len(nums) - i))
             left_sum += num
             right_sum -= num
 
