d18a832c8eee027873ec42346f39345ccb77a06e
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-04-26 12:52:48 +0200

    Update 258AddDigits.py

--- Easy/258AddDigits.py
+++ Easy/258AddDigits.py
@@ -1,16 +1,3 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-
-        while num > 9:
-            num = self.__sum_digits__(num)
-
-        return num
-
-
-    def __sum_digits__(self, num):
-        ans = 0
-        while num > 0:
-            ans += num % 10
-            num //= 10
-
-        return ans
+        return num if num < 10 else self.addDigits(int(sum(int(digit) for digit in str(num))))
