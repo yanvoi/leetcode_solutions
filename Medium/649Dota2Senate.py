33443d7b35a3d7a2b66547f8f097eb864065e2e0
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-05-04 12:53:55 +0200

    Update 649Dota2Senate.py

--- Medium/649Dota2Senate.py
+++ Medium/649Dota2Senate.py
@@ -8,17 +8,18 @@ class Solution:
         i = 0
         while r_stack and d_stack:
             cur = i % len(senate)
-            if cur not in banned:
-                helper = (cur + 1) % len(senate)
-                while helper in banned or senate[helper] == senate[cur]:
-                    helper = (helper + 1) % len(senate)
-
-                banned.add(helper)
-                if senate[cur] == "R":
-                    d_stack.pop(d_stack.index(helper))
-                else:
-                    r_stack.pop(r_stack.index(helper))
-
             i += 1
+            if cur in banned:
+                continue
+            
+            helper = (cur + 1) % len(senate)
+            while helper in banned or senate[helper] == senate[cur]:
+                helper = (helper + 1) % len(senate)
+
+            banned.add(helper)
+            if senate[cur] == "R":
+                d_stack.pop(d_stack.index(helper))
+            else:
+                r_stack.pop(r_stack.index(helper))
 
         return "Radiant" if r_stack else "Dire"
