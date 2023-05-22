20699116d808b462c18c40aa71adfa37ae2fb349
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-05-22 20:52:05 +0200

    Update 347TopKFrequentElements.py

--- Medium/347TopKFrequentElements.py
+++ Medium/347TopKFrequentElements.py
@@ -1,25 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        # creating a dictionary of how many times each number appears in the array
-        h = dict()
-        for num in nums:
-            h[num] = h.get(num, 0) + 1
-                
+        counter = collections.Counter(nums)
         # creating a dictionary of which elements appeared x times
-        freq = dict()
-        for el in h:
-            freq[h[el]] = freq.get(h[el], []) + [el]
+        frequencies = defaultdict(list)
+        for num, freq in counter.items():
+            frequencies[freq].append(num)
                 
-        # checking from the biggest possible frequency to the lowest
-        ans = []
-        for i in range(len(nums), 0, -1):
-            if i in freq:
-                
-                for x in freq[i]:
-                    ans.append(x)
-                if len(ans) >= k:
-                    return ans[:k]
+        answer = []
+        # checking from the highest possible frequency to the lowest
+        for freq in range(len(nums), 0, -1):
+            for num in frequencies[freq]:
+                answer.append(num)
+            if len(answer) >= k:
+                return answer[:k]
                     
-        return ans[:k]
+        return answer[:k]
         
