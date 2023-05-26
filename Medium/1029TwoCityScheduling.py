e5c67c7f9fdeb2618be85b5a9d99d2f88e0d305c
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-05-26 19:10:19 +0200

    Update 1029TwoCityScheduling.py

--- Medium/1029TwoCityScheduling.py
+++ Medium/1029TwoCityScheduling.py
@@ -2,6 +2,7 @@ class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
 
         a_cost = sum(cost for cost, _ in costs)
+        # treat this list as possible "refunds" you'll get when changing i^th person's city
         relocate_to_b = sorted(bcost - acost for acost, bcost in costs)
 
         return a_cost + sum(relocate_to_b[:len(costs) // 2])
