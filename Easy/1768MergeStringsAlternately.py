258eacbf8071872b0b9bac3cdd87f2c26ffc98f6
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-07-23 09:47:44 +0200

    Create 1768MergeStringsAlternately.py

---
+++ Easy/1768MergeStringsAlternately.py
@@ -0,0 +1,4 @@
+# new trick learned - zip_longest
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return "".join(a + b for a, b in zip_longest(word1, word2, fillvalue=""))
