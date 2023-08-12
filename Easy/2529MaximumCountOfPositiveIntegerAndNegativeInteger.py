a6f3082d21a1628777b44002fdf1e43fe96f6079
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-08-12 10:26:45 +0200

    Create 2529MaximumCountOfPositiveIntegerAndNegativeInteger.py

---
+++ Easy/2529MaximumCountOfPositiveIntegerAndNegativeInteger.py
@@ -0,0 +1,4 @@
+# - O(log n) time, O(1) space
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        return max(bisect.bisect_left(nums, 0), len(nums) - bisect.bisect_right(nums, 0))
