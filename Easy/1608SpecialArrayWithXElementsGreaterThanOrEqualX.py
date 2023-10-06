62a0a3d50cdb7e00073cb430f351c100842d7eba
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-10-06 21:36:52 +0200

    Create 1608SpecialArrayWithXElementsGreaterThanOrEqualX.py

---
+++ Easy/1608SpecialArrayWithXElementsGreaterThanOrEqualX.py
@@ -0,0 +1,14 @@
+# O(nlogn) time, O(1) space
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        left, right = 1, len(nums)
+        while left <= right:
+            mid = (left + right) // 2
+            count = sum(num >= mid for num in nums)
+            if count == mid: return mid
+            elif count > mid:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
