6afaf764f57f4b2ca9acc6b86ccabd97cd194c16
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-11-02 17:23:40 +0100

    Update 2917FindTheK-OrOfAnArray.py

--- Easy/2917FindTheK-OrOfAnArray.py
+++ Easy/2917FindTheK-OrOfAnArray.py
@@ -1,11 +1,11 @@
 class Solution:
     def findKOr(self, nums: List[int], k: int) -> int:
-        count = Counter()
+        bits_set_count = [0] * 31
         for num in nums:
             i = 0
             while num:
-                count[i] += num & 1
+                bits_set_count[i] += num & 1
                 num >>= 1
                 i += 1
 
-        return sum(2 ** key for key, val in count.items() if val >= k)
+        return sum(2 ** i for i, count in enumerate(bits_set_count) if count >= k)
