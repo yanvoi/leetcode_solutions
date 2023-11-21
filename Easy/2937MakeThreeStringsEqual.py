e23e3e95ec90b7ebd06cc7b7ef732b4a08c8d01f
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2023-11-21 13:35:23 +0100

    Create 2937MakeThreeStringsEqual.py

---
+++ Easy/2937MakeThreeStringsEqual.py
@@ -0,0 +1,9 @@
+# O(n) time, O(1) space
+class Solution:
+    def findMinimumOperations(self, s1: str, s2: str, s3: str) -> int:
+        same_prefix_count = 0
+        for c1, c2, c3 in zip(s1, s2, s3):
+            if c1 == c2 == c3: same_prefix_count += 1
+            else: break
+
+        return len(s1) + len(s2) + len(s3) - 3 * same_prefix_count if same_prefix_count >= 1 else -1
