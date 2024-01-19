cf1a79b46c39fe783d77729893978df3487ac8af
Jan Wojciechowski <96974442+yanvoi@users.noreply.github.com>
2024-01-19 22:58:28 +0100

    Create 2109AddingSpacesToAString.py

---
+++ Medium/2109AddingSpacesToAString.py
@@ -0,0 +1,59 @@
+"""
+You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
+
+	For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+
+Return the modified string after the spaces have been added.
+ 
+Example 1:
+Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+Output: "Leetcode Helps Me Learn"
+Explanation: 
+The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+We then place spaces before those characters.
+
+Example 2:
+Input: s = "icodeinpython", spaces = [1,5,7,9]
+Output: "i code in py thon"
+Explanation:
+The indices 1, 5, 7, and 9 correspond to the underlined characters in "icodeinpython".
+We then place spaces before those characters.
+
+Example 3:
+Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+Output: " s p a c i n g"
+Explanation:
+We are also able to place spaces before the first character of the string.
+
+ 
+Constraints:
+
+	1 <= s.length <= 3 * 10^5
+	s consists only of lowercase and uppercase English letters.
+	1 <= spaces.length <= 3 * 10^5
+	0 <= spaces[i] <= s.length - 1
+	All the values of spaces are strictly increasing.
+
+"""
+
+# O(n) time
+# it ended up looking overcomplicated but it's pretty intuitive
+# insert the spaces first and then fill in the rest with s characters
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        answer = [None] * (len(s) + len(spaces))
+        # inserting spaces (+ i because answer has different len)
+        for i, space_index in enumerate(spaces):
+            answer[space_index + i] = " "
+        
+        # insert the rest of the characters
+        result_index, char_index = 0, 0
+        while result_index < len(answer):
+            if answer[result_index]:
+                result_index += 1
+                continue
+            answer[result_index] = s[char_index]
+            result_index += 1
+            char_index += 1
+
+        return "".join(answer)
